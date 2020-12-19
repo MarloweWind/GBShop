29cd772feee1b690a456e9a01297f634d2d371b0
Alexey Malkov <marlowe.wind@gmail.com>
2020-12-19 13:19:42 +0300

    main

---
+++ GBShop/GBShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Алексей Мальков on 19.12.2020.
+//  Copyright © 2020 Alexey Malkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
